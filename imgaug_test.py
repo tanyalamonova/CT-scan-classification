af552654b0824577e1b630b0c26d279463c682a5
tanyalamonova <t.s.lamonova@gmail.com>
2019-09-24 21:41:50 +0700

    Add files via upload

---
+++ imgaug_test.py
@@ -0,0 +1,55 @@
+import glob
+import cv2
+import imgaug as ia
+from imgaug import augmenters as iaa
+import numpy as np
+import imageio
+
+ia.seed(1)
+
+seq = iaa.Sequential(
+    [
+        iaa.Fliplr(0.5),                                  
+        iaa.Sometimes(0.5, iaa.GaussianBlur(sigma=(0.1, 3.0))),     
+        iaa.ContrastNormalization((0.5, 1.5)),                   
+        iaa.AdditiveGaussianNoise(
+            loc=0, scale=(0.0, 0.1 * 255), per_channel=0),     
+        iaa.Multiply((0.8, 1.2), per_channel=0),                
+        iaa.Affine(
+            scale={
+                "x": (0.8, 1.2),
+                "y": (0.8, 1.2)
+            },
+            translate_percent={
+                "x": (-0.1, 0.1),
+                "y": (-0.1, 0.1)
+            },
+            rotate=(-10, 10))                                       
+    ],
+    random_order=True)
+
+
+
+#region mult images
+image_list = [cv2.imread(file) for file in glob.glob('test/abnormal/*.png')]
+for index, img in enumerate(image_list):
+    images = np.array([img for _ in range(64)], dtype=np.uint8) 
+    images_aug = seq.augment_images(images)
+    for i in range(64):
+        imageio.imwrite('test/abnormal/'+str(index)+'_'+str(i)+'new.png', images_aug[i])
+#endregion
+
+
+# img = imageio.imread("training_data_ct/normal/5.png") #read you image
+# images = np.array(
+#     [img for _ in range(5)], dtype=np.uint8)  # 32 means creat 32 enhanced images using following methods.
+
+# images_aug = iaa.Multiply((0.8, 1.2), per_channel=1).augment_images(images)
+# for i in range(5):
+#     imageio.imwrite('training_data_aug/test/_shear_' + str(i) + '_.png', images_aug[i])  #write all changed images
+
+
+# images_aug = seq.augment_images(images)
+
+# for i in range(4):
+#     imageio.imwrite('training_data_aug/normal/'+str(i)+'new.png', images_aug[i])  #write all changed images
---
+++ use_model.py
@@ -0,0 +1,47 @@
+from keras.models import model_from_json
+import cv2
+import glob
+import numpy as np
+
+
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+'''loaded_model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+'''
+loaded_model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+image_list = [cv2.imread(file) for file in glob.glob('test/normal/*.png')]
+
+
+img = cv2.imread("./test/normal/072.png")
+img = cv2.resize(img, (50,50))
+print(img.shape)
+img = img.reshape(1, 50, 50, 3)
+
+print(img.shape)
+print(np.argmax(loaded_model.predict(img, verbose=1)))
+print(loaded_model.predict(img))
+
+
+
+# print("normal scans")
+# right_counter = 0
+# for index, img in enumerate(image_list):
+#     img = cv2.resize(img, (50,50))
+#     # print(img.shape)
+#     img = img.reshape(1, 50, 50, 3)
+#     result = np.argmax(loaded_model.predict(img))
+#     # if int(result) == 1:
+#     #     right_counter += 1
+#     #print(img.shape)
+#     print(result)
+#     print(loaded_model.predict(img))
+# #print(right_counter)
