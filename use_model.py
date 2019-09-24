59c483b5622028b93e65328e12d3e35ba5cd1274
tanyalamonova <t.s.lamonova@gmail.com>
2019-09-24 21:48:35 +0700

    Update use_model.py

--- use_model.py
+++ use_model.py
@@ -21,7 +21,7 @@ loaded_model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics =
 image_list = [cv2.imread(file) for file in glob.glob('test/normal/*.png')]
 
 
-img = cv2.imread("./test/normal/072.png")
+img = cv2.imread("test/normal/2_63new.png")
 img = cv2.resize(img, (50,50))
 print(img.shape)
 img = img.reshape(1, 50, 50, 3)
